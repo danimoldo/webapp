f5749754a3b45a633d2939d40af837137be7b85f
danimoldo <m.dani@me.com>
2025-09-25 15:15:11 +0300

    Add files via upload

--- js/state.js
+++ js/state.js
@@ -1,14 +1,1 @@
-// js/state.js
-export function initState() {
-  return {
-    paused: false,
-    filters: new Set(["all"]),
-    assets: [],
-    extinguishers: [],
-    selectedId: null,
-    zones: [], // array of polygons [[{x,y},...], ...]
-    floorImage: null,
-    pxPerMeter: 3, // tune to your floor plan scale
-    lastTick: performance.now(),
-  };
-}
+// state.js placeholder
